84cf1abddd8cfeaf5c6e41e7ea6be9b08caec4e6
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2024-03-31 21:53:55 +0600

    added recursion in java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello world");
+        message2();
+        System.out.println("print last ");
+    }
+
+    static void message2() {
+        System.out.println("message 2");
+    }
+}
